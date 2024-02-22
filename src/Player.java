82f5ab599e86f93683ba1819944ee4b013f36584
VitBuk <vitbuk@gmail.com>
2024-02-22 16:42:26 +0200

    Player entity added

---
+++ src/Player.java
@@ -0,0 +1,35 @@
+import java.util.List;
+
+public class Player {
+    private String name;
+    private List<String> currentList;
+    private int guess;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public int getGuess() {
+        return guess;
+    }
+
+    public void setGuess(int guess) {
+        this.guess = guess;
+    }
+
+    public List<String> getCurrentList() {
+        return currentList;
+    }
+
+    public void setCurrentList(List<String> currentList) {
+        this.currentList = currentList;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
