d3a572115b9ad00f88717bf8443370c2ecd6b7a6
VitBuk <vitbuk@gmail.com>
2024-02-29 16:08:20 +0200

    checkResult method added to interface twoplayersStratefy

--- src/vitbuk/strategies/TwoPlayersStrategy.java
+++ src/vitbuk/strategies/TwoPlayersStrategy.java
@@ -3,6 +3,10 @@ package vitbuk.strategies;
 import vitbuk.Casino;
 import vitbuk.Player;
 
+import java.util.List;
+
 public interface TwoPlayersStrategy {
     public void execute(Casino casino, Player playerA, Player playerB);
+
+    public boolean checkResult(List<String> listOfTosses, Player playerA, Player playerB);
 }
