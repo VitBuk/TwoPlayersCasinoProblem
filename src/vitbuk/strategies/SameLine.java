628c4f7777fdbce7c6420596520fbe75210f72cc
VitBuk <vitbuk@gmail.com>
2024-02-29 16:54:27 +0200

    SameLine strategy fully implemented

--- src/vitbuk/strategies/SameLine.java
+++ src/vitbuk/strategies/SameLine.java
@@ -5,7 +5,6 @@ import vitbuk.Casino;
 import vitbuk.Constants;
 import vitbuk.Player;
 
-import java.util.List;
 import java.util.Random;
 
 public class SameLine implements TwoPlayersStrategy {
@@ -13,12 +12,22 @@ public class SameLine implements TwoPlayersStrategy {
     public void execute(Casino casino, Player playerA, Player playerB){
         Random random = new Random();
         int randNumber;
+        int rightAmount = 0;
+        int wrongAmount = 0;
 
-        for (int i=0; i<Constants.coinTossAmount; i++) {
+        for (int i=0; i<Constants.simulationAmount; i++) {
             randNumber = random.nextInt(Constants.coinTossAmount);
             playerA.setGuess(randNumber);
             playerB.setGuess(randNumber);
+            playerA.setCurrentList(casino.getNewListOfTosses());
+            playerB.setCurrentList(casino.getNewListOfTosses());
+            System.out.println(casino.checkGuesses(playerA, playerB));
+            if (casino.checkGuesses(playerA, playerB)) rightAmount++;
+            else wrongAmount++;
         }
+
+        System.out.println("Right amount: " + rightAmount);
+        System.out.println("Wrong amount: " + wrongAmount);
     };
 
 }
