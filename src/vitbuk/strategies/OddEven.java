39ff0e2e9766300bc36bbcc8b66d08c4165cf2e5
VitBuk <vitbuk@gmail.com>
2024-02-29 15:37:17 +0200

    OddEven strategy class created

---
+++ src/vitbuk/strategies/OddEven.java
@@ -0,0 +1,10 @@
+package vitbuk.strategies;
+
+import vitbuk.Player;
+
+public class OddEven implements TwoPlayersStrategy{
+    @Override
+    public void execute(Player playerA, Player playerB) {
+
+    }
+}
